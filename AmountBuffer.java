d5932d648fe928c54cef79a828735dfd7b1f8ecb
Fred Gravil <fgravil18@knights.ucf.edu>
2016-02-13 22:46:12 -0500

    Multithreaded Bank Application

---
+++ AmountBuffer.java
@@ -0,0 +1,10 @@
+package fred.gravil.com;
+
+/**
+ * Created by Fred on 2/11/2016.
+ */
+public interface AmountBuffer {
+
+    public void deposit( String name,int amount);
+    public void withdraw(String name,int amount);
+}
